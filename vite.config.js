1989fafefce22b42a7eec1e2686edb8b9c9bd94b
mr.sachin patil <102552657+SACHIN-CREATION@users.noreply.github.com>
2025-07-29 18:40:46 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/MoryaCarsDemo/', // <-- Add this line
+  plugins: [react()],
+   base: '/MoryaCarsDemo/', // 
 })
